d6a31302462573f1d457f143df266b0e25441a76
Alejandro <alemaldo@gmail.com>
2014-07-17 13:25:12 -0300

    Avance ABM Empresa

--- TP/Aplicacion Desktop/FrbaCommerce/FrbaCommerce/Abm Cliente/Listado.Designer.cs
+++ TP/Aplicacion Desktop/FrbaCommerce/FrbaCommerce/Abm Cliente/Listado.Designer.cs
@@ -50,10 +50,27 @@
             this.txtNombre = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.tabPageEmpresa = new System.Windows.Forms.TabPage();
+            this.btnBuscarEmp = new System.Windows.Forms.Button();
+            this.lnkLimpiarEmp = new System.Windows.Forms.LinkLabel();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtMailEmp = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtCUITEmp = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnHabilitarEmp = new System.Windows.Forms.Button();
+            this.btnInhabilitarEmp = new System.Windows.Forms.Button();
+            this.btnBajaEmp = new System.Windows.Forms.Button();
+            this.btnModificarEmp = new System.Windows.Forms.Button();
+            this.btnCrearEmp = new System.Windows.Forms.Button();
+            this.dataGridEmpresa = new System.Windows.Forms.DataGridView();
+            this.txtEmpRazon = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
             this.tabPageAdmin = new System.Windows.Forms.TabPage();
             this.tabUsuarios.SuspendLayout();
             this.tabPageCliente.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridClientes)).BeginInit();
+            this.tabPageEmpresa.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridEmpresa)).BeginInit();
             this.SuspendLayout();
             // 
             // tabUsuarios
@@ -191,7 +208,6 @@
             // 
             this.dataGridClientes.AllowUserToAddRows = false;
             this.dataGridClientes.AllowUserToDeleteRows = false;
-            this.dataGridClientes.AllowUserToResizeColumns = false;
             this.dataGridClientes.AllowUserToResizeRows = false;
             this.dataGridClientes.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridClientes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
@@ -296,23 +312,195 @@
             // 
             // tabPageEmpresa
             // 
+            this.tabPageEmpresa.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.tabPageEmpresa.Controls.Add(this.btnBuscarEmp);
+            this.tabPageEmpresa.Controls.Add(this.lnkLimpiarEmp);
+            this.tabPageEmpresa.Controls.Add(this.label10);
+            this.tabPageEmpresa.Controls.Add(this.txtMailEmp);
+            this.tabPageEmpresa.Controls.Add(this.label9);
+            this.tabPageEmpresa.Controls.Add(this.txtCUITEmp);
+            this.tabPageEmpresa.Controls.Add(this.label7);
+            this.tabPageEmpresa.Controls.Add(this.btnHabilitarEmp);
+            this.tabPageEmpresa.Controls.Add(this.btnInhabilitarEmp);
+            this.tabPageEmpresa.Controls.Add(this.btnBajaEmp);
+            this.tabPageEmpresa.Controls.Add(this.btnModificarEmp);
+            this.tabPageEmpresa.Controls.Add(this.btnCrearEmp);
+            this.tabPageEmpresa.Controls.Add(this.dataGridEmpresa);
+            this.tabPageEmpresa.Controls.Add(this.txtEmpRazon);
+            this.tabPageEmpresa.Controls.Add(this.label6);
             this.tabPageEmpresa.Location = new System.Drawing.Point(4, 25);
             this.tabPageEmpresa.Name = "tabPageEmpresa";
             this.tabPageEmpresa.Padding = new System.Windows.Forms.Padding(3);
             this.tabPageEmpresa.Size = new System.Drawing.Size(702, 459);
             this.tabPageEmpresa.TabIndex = 1;
             this.tabPageEmpresa.Text = "Usuarios Empresa";
-            this.tabPageEmpresa.UseVisualStyleBackColor = true;
+            // 
+            // btnBuscarEmp
+            // 
+            this.btnBuscarEmp.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBuscarEmp.Location = new System.Drawing.Point(591, 101);
+            this.btnBuscarEmp.Name = "btnBuscarEmp";
+            this.btnBuscarEmp.Size = new System.Drawing.Size(88, 30);
+            this.btnBuscarEmp.TabIndex = 65;
+            this.btnBuscarEmp.Text = "Buscar";
+            this.btnBuscarEmp.UseVisualStyleBackColor = true;
+            this.btnBuscarEmp.Click += new System.EventHandler(this.btnBuscarEmp_Click);
+            // 
+            // lnkLimpiarEmp
+            // 
+            this.lnkLimpiarEmp.AutoSize = true;
+            this.lnkLimpiarEmp.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lnkLimpiarEmp.Location = new System.Drawing.Point(570, 24);
+            this.lnkLimpiarEmp.Name = "lnkLimpiarEmp";
+            this.lnkLimpiarEmp.Size = new System.Drawing.Size(109, 17);
+            this.lnkLimpiarEmp.TabIndex = 64;
+            this.lnkLimpiarEmp.TabStop = true;
+            this.lnkLimpiarEmp.Text = "Limpiar Campos";
+            this.lnkLimpiarEmp.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lnkLimpiarEmp_LinkClicked);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(19, 24);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(279, 17);
+            this.label10.TabIndex = 63;
+            this.label10.Text = "Seleccione el filtro y presione Buscar";
+            // 
+            // txtMailEmp
+            // 
+            this.txtMailEmp.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMailEmp.Location = new System.Drawing.Point(148, 105);
+            this.txtMailEmp.Name = "txtMailEmp";
+            this.txtMailEmp.Size = new System.Drawing.Size(129, 23);
+            this.txtMailEmp.TabIndex = 62;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(18, 108);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(124, 17);
+            this.label9.TabIndex = 61;
+            this.label9.Text = "Correo electronico";
+            // 
+            // txtCUITEmp
+            // 
+            this.txtCUITEmp.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCUITEmp.Location = new System.Drawing.Point(330, 66);
+            this.txtCUITEmp.Name = "txtCUITEmp";
+            this.txtCUITEmp.Size = new System.Drawing.Size(129, 23);
+            this.txtCUITEmp.TabIndex = 60;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(285, 69);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(39, 17);
+            this.label7.TabIndex = 59;
+            this.label7.Text = "CUIT";
+            // 
+            // btnHabilitarEmp
+            // 
+            this.btnHabilitarEmp.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHabilitarEmp.Location = new System.Drawing.Point(421, 413);
+            this.btnHabilitarEmp.Name = "btnHabilitarEmp";
+            this.btnHabilitarEmp.Size = new System.Drawing.Size(125, 30);
+            this.btnHabilitarEmp.TabIndex = 58;
+            this.btnHabilitarEmp.Text = "Habilitar";
+            this.btnHabilitarEmp.UseVisualStyleBackColor = true;
+            this.btnHabilitarEmp.Click += new System.EventHandler(this.btnHabilitarEmp_Click);
+            // 
+            // btnInhabilitarEmp
+            // 
+            this.btnInhabilitarEmp.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInhabilitarEmp.Location = new System.Drawing.Point(288, 413);
+            this.btnInhabilitarEmp.Name = "btnInhabilitarEmp";
+            this.btnInhabilitarEmp.Size = new System.Drawing.Size(125, 30);
+            this.btnInhabilitarEmp.TabIndex = 57;
+            this.btnInhabilitarEmp.Text = "Inhabilitar";
+            this.btnInhabilitarEmp.UseVisualStyleBackColor = true;
+            this.btnInhabilitarEmp.Click += new System.EventHandler(this.btnInhabilitarEmp_Click);
+            // 
+            // btnBajaEmp
+            // 
+            this.btnBajaEmp.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBajaEmp.Location = new System.Drawing.Point(554, 413);
+            this.btnBajaEmp.Name = "btnBajaEmp";
+            this.btnBajaEmp.Size = new System.Drawing.Size(125, 30);
+            this.btnBajaEmp.TabIndex = 56;
+            this.btnBajaEmp.Text = "Dar de Baja";
+            this.btnBajaEmp.UseVisualStyleBackColor = true;
+            this.btnBajaEmp.Click += new System.EventHandler(this.btnBajaEmp_Click);
+            // 
+            // btnModificarEmp
+            // 
+            this.btnModificarEmp.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnModificarEmp.Location = new System.Drawing.Point(155, 413);
+            this.btnModificarEmp.Name = "btnModificarEmp";
+            this.btnModificarEmp.Size = new System.Drawing.Size(125, 30);
+            this.btnModificarEmp.TabIndex = 55;
+            this.btnModificarEmp.Text = "Modificar";
+            this.btnModificarEmp.UseVisualStyleBackColor = true;
+            this.btnModificarEmp.Click += new System.EventHandler(this.btnModificarEmp_Click);
+            // 
+            // btnCrearEmp
+            // 
+            this.btnCrearEmp.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCrearEmp.Location = new System.Drawing.Point(22, 413);
+            this.btnCrearEmp.Name = "btnCrearEmp";
+            this.btnCrearEmp.Size = new System.Drawing.Size(125, 30);
+            this.btnCrearEmp.TabIndex = 54;
+            this.btnCrearEmp.Text = "Crear";
+            this.btnCrearEmp.UseVisualStyleBackColor = true;
+            this.btnCrearEmp.Click += new System.EventHandler(this.btnCrearEmp_Click);
+            // 
+            // dataGridEmpresa
+            // 
+            this.dataGridEmpresa.AllowUserToAddRows = false;
+            this.dataGridEmpresa.AllowUserToDeleteRows = false;
+            this.dataGridEmpresa.AllowUserToResizeRows = false;
+            this.dataGridEmpresa.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridEmpresa.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridEmpresa.Location = new System.Drawing.Point(21, 150);
+            this.dataGridEmpresa.MultiSelect = false;
+            this.dataGridEmpresa.Name = "dataGridEmpresa";
+            this.dataGridEmpresa.ReadOnly = true;
+            this.dataGridEmpresa.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridEmpresa.Size = new System.Drawing.Size(658, 257);
+            this.dataGridEmpresa.TabIndex = 53;
+            // 
+            // txtEmpRazon
+            // 
+            this.txtEmpRazon.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmpRazon.Location = new System.Drawing.Point(116, 63);
+            this.txtEmpRazon.Name = "txtEmpRazon";
+            this.txtEmpRazon.Size = new System.Drawing.Size(161, 23);
+            this.txtEmpRazon.TabIndex = 37;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(19, 66);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(91, 17);
+            this.label6.TabIndex = 36;
+            this.label6.Text = "Razon Social";
             // 
             // tabPageAdmin
             // 
+            this.tabPageAdmin.BackColor = System.Drawing.Color.WhiteSmoke;
             this.tabPageAdmin.Location = new System.Drawing.Point(4, 25);
             this.tabPageAdmin.Name = "tabPageAdmin";
             this.tabPageAdmin.Padding = new System.Windows.Forms.Padding(3);
             this.tabPageAdmin.Size = new System.Drawing.Size(702, 459);
             this.tabPageAdmin.TabIndex = 2;
             this.tabPageAdmin.Text = "Administradores";
-            this.tabPageAdmin.UseVisualStyleBackColor = true;
             // 
             // Listado
             // 
@@ -327,6 +515,9 @@
             this.tabPageCliente.ResumeLayout(false);
             this.tabPageCliente.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridClientes)).EndInit();
+            this.tabPageEmpresa.ResumeLayout(false);
+            this.tabPageEmpresa.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridEmpresa)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -356,6 +547,21 @@
         private System.Windows.Forms.LinkLabel linkLabel1;
         private System.Windows.Forms.Button btnInhabilitar;
         private System.Windows.Forms.Button btnHabilitar;
+        private System.Windows.Forms.Button btnHabilitarEmp;
+        private System.Windows.Forms.Button btnInhabilitarEmp;
+        private System.Windows.Forms.Button btnBajaEmp;
+        private System.Windows.Forms.Button btnModificarEmp;
+        private System.Windows.Forms.Button btnCrearEmp;
+        private System.Windows.Forms.DataGridView dataGridEmpresa;
+        private System.Windows.Forms.TextBox txtEmpRazon;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtCUITEmp;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnBuscarEmp;
+        private System.Windows.Forms.LinkLabel lnkLimpiarEmp;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txtMailEmp;
+        private System.Windows.Forms.Label label9;
 
 
     }
--- TP/Aplicacion Desktop/FrbaCommerce/FrbaCommerce/Abm Cliente/Listado.cs
+++ TP/Aplicacion Desktop/FrbaCommerce/FrbaCommerce/Abm Cliente/Listado.cs
@@ -91,6 +91,8 @@ namespace FrbaCommerce.Abm_Cliente
             dataGridClientes.AutoGenerateColumns = true;
             dataGridClientes.DataSource = datos.Tables[0];
 
+            dataGridClientes.Columns[0].Visible = false;
+
             // Cierro la conexion
             AccesoDatos.getInstancia().cerrarConexion();
             
@@ -123,7 +125,7 @@ namespace FrbaCommerce.Abm_Cliente
             
             int id_usuario = Convert.ToInt32(this.dataGridClientes.SelectedRows[0].Cells[0].Value);
 
-            if (MessageBox.Show("¿Confirma la eliminacion del usuario?", "Eliminacion", MessageBoxButtons.YesNo)
+            if (MessageBox.Show("¿Confirma la baja del usuario?", "Baja", MessageBoxButtons.YesNo)
                    == DialogResult.Yes)
             {
                 string sql_qry = "UPDATE LOS_GESTORES.Usuario SET id_estado = (select id_estado from LOS_GESTORES.Estado where descripcion = 'Baja') " +
@@ -202,8 +204,158 @@ namespace FrbaCommerce.Abm_Cliente
             this.btnBuscar.PerformClick();
         }
 
+        private void lnkLimpiarEmp_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            this.txtCUITEmp.Clear();
+            this.txtEmpRazon.Clear();
+            this.txtMailEmp.Clear();
+        }
+
+        private void btnBuscarEmp_Click(object sender, EventArgs e)
+        {
+            string sql_qry = "SELECT U.id_usuario id, U.nombre_usuario Usuario, UE.razon_social Razon," +
+                                " UE.cuit, E.descripcion Estado" +
+                                " FROM LOS_GESTORES.Usuario_Empresa UE" +
+                                " JOIN LOS_GESTORES.Usuario U on U.id_usuario = UE.id_usuario" +
+                                " JOIN LOS_GESTORES.Estado E on E.id_estado = U.id_estado" +
+                                " JOIN LOS_GESTORES.Domicilio D on D.id_domicilio = UE.id_domicilio WHERE 1=1";
+
+            //Armo el string de acuerdo a los parametros de busqueda
+            if (txtEmpRazon.Text.Trim() != string.Empty)
+            {
+                sql_qry = sql_qry + " AND UE.razon_social like '%" + txtEmpRazon.Text.Trim() + "%'";
+            }
+
+            if (txtCUITEmp.Text.Trim() != string.Empty)
+            {
+                sql_qry = sql_qry + " AND UE.cuit = '" + txtCUITEmp.Text.Trim() + "'";
+            }
+
+            if (txtMailEmp.Text.Trim() != string.Empty)
+            {
+                sql_qry = sql_qry + " AND D.Mail like '%" + txtMailEmp.Text.Trim() + "%'";
+            }
+
+
+            // Abro la conexion
+            AccesoDatos.getInstancia().abrirConexion();
+
+            DataSet datos = AccesoDatos.getInstancia().consultaSimple(sql_qry);
+
+            dataGridEmpresa.AutoGenerateColumns = true;
+            dataGridEmpresa.DataSource = datos.Tables[0];
+
+            dataGridEmpresa.Columns[0].Visible = false;
+
+            // Cierro la conexion
+            AccesoDatos.getInstancia().cerrarConexion();        
+
+        }
+
+        private void btnInhabilitarEmp_Click(object sender, EventArgs e)
+        {
+
+            if (dataGridEmpresa.SelectedRows.Count != 1) return;
+
+            int id_usuario = Convert.ToInt32(this.dataGridEmpresa.SelectedRows[0].Cells[0].Value);
+
+            if (MessageBox.Show("¿Confirma la Inhabilitacion del usuario?", "Inhabilitacion", MessageBoxButtons.YesNo)
+                   == DialogResult.Yes)
+            {
+                string sql_qry = "UPDATE LOS_GESTORES.Usuario SET id_estado = (select id_estado from LOS_GESTORES.Estado where descripcion = 'Inhabilitado') " +
+                    " WHERE id_usuario = " + id_usuario.ToString();
+
+                // Abro la conexion
+                AccesoDatos.getInstancia().abrirConexion();
+
+                DataSet datos = AccesoDatos.getInstancia().consultaSimple(sql_qry);
+
+                // Cierro la conexion
+                AccesoDatos.getInstancia().cerrarConexion();
+
+                this.btnBuscarEmp.PerformClick();
+
+            }
+
+
+        }
+
+        private void btnHabilitarEmp_Click(object sender, EventArgs e)
+        {
+            if (dataGridEmpresa.SelectedRows.Count != 1) return;
+
+            int id_usuario = Convert.ToInt32(this.dataGridEmpresa.SelectedRows[0].Cells[0].Value);
+
+            if (MessageBox.Show("¿Confirma la Habilitacion del usuario?", "Habilitacion", MessageBoxButtons.YesNo)
+                   == DialogResult.Yes)
+            {
+                string sql_qry = "UPDATE LOS_GESTORES.Usuario SET id_estado = (select id_estado from LOS_GESTORES.Estado where descripcion = 'Habilitado') " +
+                    " WHERE id_usuario = " + id_usuario.ToString();
+
+                // Abro la conexion
+                AccesoDatos.getInstancia().abrirConexion();
+
+                DataSet datos = AccesoDatos.getInstancia().consultaSimple(sql_qry);
+
+                // Cierro la conexion
+                AccesoDatos.getInstancia().cerrarConexion();
+
+                this.btnBuscarEmp.PerformClick();
+
+            }
+
+        }
+
+        private void btnBajaEmp_Click(object sender, EventArgs e)
+        {
+            if (dataGridEmpresa.SelectedRows.Count != 1) return;
+
+            int id_usuario = Convert.ToInt32(this.dataGridEmpresa.SelectedRows[0].Cells[0].Value);
+
+            if (MessageBox.Show("¿Confirma la baja del usuario?", "Baja", MessageBoxButtons.YesNo)
+                   == DialogResult.Yes)
+            {
+                string sql_qry = "UPDATE LOS_GESTORES.Usuario SET id_estado = (select id_estado from LOS_GESTORES.Estado where descripcion = 'Baja') " +
+                    " WHERE id_usuario = " + id_usuario.ToString();
+
+                // Abro la conexion
+                AccesoDatos.getInstancia().abrirConexion();
+
+                DataSet datos = AccesoDatos.getInstancia().consultaSimple(sql_qry);
+
+                // Cierro la conexion
+                AccesoDatos.getInstancia().cerrarConexion();
+
+                this.btnBuscarEmp.PerformClick();
+
+            }
+
+        }
+
+        private void btnCrearEmp_Click(object sender, EventArgs e)
+        {
+            Abm_Cliente.AMCliente fCli = new AMCliente();
+            fCli.ShowDialog();
+            this.btnBuscar.PerformClick();
+        }
+
+        private void btnModificarEmp_Click(object sender, EventArgs e)
+        {
+            if (dataGridClientes.SelectedRows.Count != 1) return;
+
+            int id_usuario = Convert.ToInt32(this.dataGridClientes.SelectedRows[0].Cells[0].Value);
+
+            Abm_Cliente.AMCliente fCli = new AMCliente();
+            fCli.idusuario = id_usuario;
+            fCli.ShowDialog();
+            this.btnBuscar.PerformClick();
+        }
+
         
 
+        
+
+        
 
 
     }
