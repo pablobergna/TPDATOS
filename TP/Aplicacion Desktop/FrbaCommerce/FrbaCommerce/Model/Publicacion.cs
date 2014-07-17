f070b1e322b98d4f310aee1bf2bf67d929a175e5
pablobergna <pablo.bergna@gmail.com>
2014-07-17 01:05:07 -0300

    agregada clase publicacion
    la agrego de nuevo porque no me queria compilar (god knows why)

--- TP/Aplicacion Desktop/FrbaCommerce/FrbaCommerce/Model/Publicacion.cs
+++ TP/Aplicacion Desktop/FrbaCommerce/FrbaCommerce/Model/Publicacion.cs
@@ -7,16 +7,16 @@ namespace FrbaCommerce.Model
 {
     public class Publicacion
     {
-        public int          id                  { get; set; }
-        public int          id_usuario          { get; set; }
-        public string       descripcion         { get; set; }
-        public int          tipo_publicacion    { get; set; }
-        public int          stock               { get; set; }
-        public float        precio              { get; set; }
-        public int          visibilidad         { get; set; }
-        public int          permitir_preguntas  { get; set; }
-        public DateTime     vencimiento         { get; set; }
-        public string       estado              { get; set; }
-        public Queue<int>   rubros              { get; set; }
+        public int id { get; set; }
+        public int id_usuario { get; set; }
+        public string descripcion { get; set; }
+        public int tipo_publicacion { get; set; }
+        public int stock { get; set; }
+        public float precio { get; set; }
+        public int visibilidad { get; set; }
+        public int permitir_preguntas { get; set; }
+        public DateTime vencimiento { get; set; }
+        public string estado { get; set; }
+        public Queue<int> rubros { get; set; }
     }
 }
