2ea266477ee6f437e6ce17eed782025d00b4d476
pablobergna <pablo.bergna@gmail.com>
2014-07-18 13:02:20 -0300

    sp de editar publicacion

--- TP/Stored_Procedures/SP_EP_TRAER_PUBLICACION.sql
+++ TP/Stored_Procedures/SP_EP_TRAER_PUBLICACION.sql
@@ -7,9 +7,8 @@ BEGIN
 				WHERE P.id_publicacion = @id_publicacion
 				AND P.tipo_publicacion = 1))
 	BEGIN
-		SELECT P.descripcion, P.tipo_publicacion,PUBI.stock, PUBI.precio, P.id_visibilidad, P.f_preguntas, UPPER(E.descripcion) desc_estado
+		SELECT TOP 1 ISNULL(P.descripcion, '') descripcion, P.tipo_publicacion, ISNULL(PUBI.stock,0) stock, ISNULL(PUBI.precio,0) precio, P.id_visibilidad, P.f_preguntas, UPPER(E.descripcion) desc_estado
 		FROM LOS_GESTORES.Publicacion P
-		,LOS_GESTORES.Visibilidad V
 		,LOS_GESTORES.Estado E
 		,LOS_GESTORES.Publicacion_Inmediata PUBI
 		WHERE 1=1
@@ -22,9 +21,8 @@ BEGIN
 					WHERE P.id_publicacion = @id_publicacion
 					AND P.tipo_publicacion = 2))
 	BEGIN
-		SELECT P.descripcion, P.tipo_publicacion,PS.precio_inicial, P.id_visibilidad, P.f_preguntas, UPPER(E.descripcion) desc_estado
+		SELECT TOP 1 ISNULL(P.descripcion, '') descripcion, P.tipo_publicacion, ISNULL(PS.precio_inicial,0) precio_inicial, P.id_visibilidad, P.f_preguntas, UPPER(E.descripcion) desc_estado
 		FROM LOS_GESTORES.Publicacion P
-		,LOS_GESTORES.Visibilidad V
 		,LOS_GESTORES.Estado E
 		,LOS_GESTORES.Publicacion_Subasta PS
 		WHERE 1=1
